233012caa362520528df8e751cb7d07cfba1f9e6
KrzysztofHa <112620293+KrzysztofHa@users.noreply.github.com>
2026-08-30 21:47:17 +0200

    Update NuGet packages and improve test isolation
    Updated xUnit and Newtonsoft.Json package versions in project files. Refactored SinglePlayerDuelTest to dynamically determine duelId, enhance test isolation, and clean up created items after tests.

--- Manager.Tests/ManagerAppTest/SinglePlayerDuelServiceTest/SinglePlayerDuelTest.cs
+++ Manager.Tests/ManagerAppTest/SinglePlayerDuelServiceTest/SinglePlayerDuelTest.cs
@@ -7,23 +7,28 @@ namespace Manager.Tests.ManagerAppTest.SinglePlayerDuelTest;
 public class SinglePlayerDuelTest
 {
     private readonly SinglePlayerDuelService _singlePlayerDuelService;
+    private int duelId = 0;
+    private List<SinglePlayerDuel> _singlePlayerDuels = new List<SinglePlayerDuel>();
 
     public SinglePlayerDuelTest()
     {
         _singlePlayerDuelService = new SinglePlayerDuelService();
+        _singlePlayerDuels = _singlePlayerDuelService.GetAllItem();
     }
 
     [Fact]
     public void CanStartSinglePlayerDuel()
     {
         //Arrenge
-        var duel = new SinglePlayerDuel() { IdFirstPlayer = 1, IdSecondPlayer = 2, RaceTo = 4, TypeNameOfGame = "8 balls", ModifiedById = 1 };
+        var duel = new SinglePlayerDuel() { IdSecondPlayer = 2, RaceTo = 4, TypeNameOfGame = "8 balls", ModifiedById = 1 };
+        duelId = _singlePlayerDuelService.GetAllSinglePlayerDuel().Max(d => d.Id) + 1;
 
         //Act
         _singlePlayerDuelService.StartSinglePlayerDuel(duel);
 
         //Assert
-        duel.Id.Should().Be(1).Should().NotBeNull();
+        duel.Id.Should().Be(duelId).Should().NotBeNull();
+        duel.IdSecondPlayer.Should().Be(2);
         duel.StartGame.Should().BeBefore(DateTime.Now);
     }
 
@@ -33,14 +38,14 @@ public class SinglePlayerDuelTest
         //Arrange
         CanStartSinglePlayerDuel();
         //CanStartSinglePlayerDuel();
-        var duel = _singlePlayerDuelService.GetItemById(1);
+        var duel = _singlePlayerDuelService.GetItemById(duelId);
         duel.ScoreFirstPlayer = 4;
 
         //Act
         _singlePlayerDuelService.UpdateSinglePlayerDuel(duel);
 
         //Assert
-        _singlePlayerDuelService.GetItemById(1).ScoreFirstPlayer.Should().Be(4);
+        _singlePlayerDuelService.GetItemById(duelId).ScoreFirstPlayer.Should().Be(4);
     }
 
     [Fact]
@@ -48,12 +53,14 @@ public class SinglePlayerDuelTest
     {
         // Arrange
         CanUpdateSinglePlayerDuel();
-        var duel = _singlePlayerDuelService.GetItemById(1);
+        var duel = _singlePlayerDuelService.GetItemById(duelId);
 
         //Act
         _singlePlayerDuelService.EndSinglePlayerDuel(duel);
 
         //Assert
-        _singlePlayerDuelService.GetItemById(1).EndGame.Should().BeAfter(duel.StartGame);
+        _singlePlayerDuelService.GetItemById(duelId).EndGame.Should().BeAfter(duel.StartGame);
+        // clean up list
+        _singlePlayerDuelService.RemoveItem(duel);
     }
 }
